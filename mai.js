984667b2108b995091108009785180be5c814681
Пользователь <sobirjonsharipov590@gmail.com>
2025-05-28 14:34:53 +0500

    Fool

--- mai.js
+++ mai.js
@@ -15,8 +15,19 @@ let InfoAge = document.querySelector(".InfoAge");
 let InfoScid = document.querySelector(".InfoScid");
 let InfoStatus = document.querySelector(".InfoStatus");
 let InfoClose = document.querySelector(".InfoClose");
+let Sort = document.querySelector(".Sort");
 let idx=null
 
+
+Sort.onclick = () => {
+  product.sort((a, b) => {
+    if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+    if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+    return 0;
+  });
+  localStorage.setItem("data", JSON.stringify(product));
+  getData(product);
+};
 // Addproduct.onclick=()=>{
 //   Adddialog.showModal()
 // }
@@ -84,7 +95,7 @@ let product = [
     name: "Sony PlayStation 5 ",
      scidca:"20% Скидка",
     age: "650$",
-    status: false,
+    status: true,
     img: "https://storage.alifshop.tj/media/images/alifshop/20022/igrovaya-pristavka-sony-playstation-5-marvel-s-spider-man-2-limited-edition-825-gb-1703671356807-lg.webp",
     id: "6",
   },
@@ -92,7 +103,7 @@ let product = [
     name: "Наушники HyperX Cloud 3 ",
      scidca:"20% Скидка",
     age: "105$",
-    status: false,
+    status: true,
     img: "https://storage.alifshop.tj/media/images/alifshop/25001/kompyuternye-provodnye-naushniki-hyperx-cloud-3-krasno-chernyy-1725448264884-lg.webp",
     id: "7",
   },
@@ -100,7 +111,7 @@ let product = [
     name: "Razer Blackshark V2 X  ",
      scidca:"20% Скидка",
     age: "70$",
-    status: false,
+    status: true,
     img: "https://storage.alifshop.tj/media/images/alifshop/35113/kompyuternye-naushniki-razer-blackshark-v2-x-provodnye-belyy-1744782955702-lg.webp",
     id: "8",
   },
@@ -131,7 +142,8 @@ function getData(data) {
     pSpan.innerHTML=el.scidca
     pSpan.classList.add("span")
     let pStatus = document.createElement("p");
-    pStatus.innerHTML = el.status ? "Active" : "Inactive";
+    pStatus.innerHTML = el.status ? "Есть в наличи" : "Нет в продаже";
+    pStatus.style.color = el.status ? "green" : "red";
     let img = document.createElement("img");
     img.src = el.img;
     img.classList.add("img");
@@ -139,7 +151,7 @@ function getData(data) {
     let btncart = document.createElement("button");
     btncart.innerHTML =`<svg style="width: 20px;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-        </svg>  B сорзину`;
+        </svg>  B Kорзину`;
     btncart.classList.add("cart");
     btncart.onclick = () => {
       Addtocart(el);
